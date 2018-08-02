2ae0ec77b0c545415779cc0c497ac7bb91a29aae
oracle <oracle@linux75.master>
2018-08-02 11:33:16 +0800

    update

---
+++ src/main/java/com/coherence/demo/ServletInitializer.java
@@ -0,0 +1,16 @@
+package com.coherence.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
+	// public class ServletInitializer extends SpringBootServletInitializer
+	// implements WebApplicationInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CoherenceApplication.class);
+	}
+
+}
